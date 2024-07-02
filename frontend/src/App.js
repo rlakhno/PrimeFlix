3b4f811892bfae16a39418f21ecd653cbc57b4c6
JackMac10 <macdonaldjackson0@gmail.com>
2024-07-02 08:05:51 -0700

    video player

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
-
-
-
 import './App.css';
 import Login from './Login';
 import LoginContainer from './components/LoginContainer';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Signup from './Signup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './Signup';
+import VideoLibrary from './components/VideoLibrary';
+import VideoPlayer from './components/VideoPlayer';
 
 function App() {
   return (
     <BrowserRouter>
-    <LoginContainer />
+      <LoginContainer />
       <Routes>
-          <Route path='/' element={<Login />}></Route>
-          <Route path='/signup' element={<Signup />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/library" element={<VideoLibrary />} />
+        <Route path="/video/:id" element={<VideoPlayer />} />
       </Routes>
-      
-
     </BrowserRouter>
   );
 }
