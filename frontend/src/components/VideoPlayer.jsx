52118afc9fa4f66f594e8747aec12060452b8550
JackMac10 <macdonaldjackson0@gmail.com>
2024-07-03 16:47:56 -0700

    video library running off pexels api

--- frontend/src/components/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer.jsx
@@ -1,42 +1,75 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import ReactPlayer from 'react-player';
+import '../App.css';
 
 const VideoPlayer = () => {
   const { id } = useParams();
-  const videos = [
-    { id: 1, title: 'good news', url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4' },
-    { id: 2, title: 'zoo', url: 'https://www.youtube.com/watch?v=jNQXAC9IVRw' },
-    // Add more videos as needed
-  ];
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const videoUrl = queryParams.get('url');
 
-  const video = videos.find(video => video.id === parseInt(id));
+  if (!videoUrl) {
+    return <p>Video not found</p>;
+  }
 
   return (
     <div>
-      {video ? (
-        <div>
-          <h1>{video.title}</h1>
-          <ul>
-            <li>
-              {/* className = videoplayer container */}
-          <ReactPlayer url={video.url} controls />
-            </li>
-            <li>
-              {/* className= video info container */}
-              {/* description */}
-              {/* cast / info / release time*/}
-            </li>
-            {/* <li>
-               more titles (stretch) 
-            </li> */}
-          </ul>
+      <div>
+        <h1>**title**</h1>
+        <div className="videoplayer-container">
+          <ReactPlayer url={videoUrl} controls  />
         </div>
-      ) : (
-        <p>Video not found</p>
-      )}
+        <div className="video-info-container">
+          {/* description, cast, release time*/}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default VideoPlayer;
+
+
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import ReactPlayer from 'react-player';
+
+// const VideoPlayer = () => {
+//   const { id } = useParams();
+//   const videos = [
+//     { id: 1, title: 'good news', url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4' },
+//     { id: 2, title: 'zoo', url: 'https://www.youtube.com/watch?v=jNQXAC9IVRw' },
+//     // Add more videos as needed
+//   ];
+
+//   const video = videos.find(video => video.id === parseInt(id));
+
+//   return (
+//     <div>
+//       {video ? (
+//         <div>
+//           <h1>{video.title}</h1>
+//           <ul>
+//             <li>
+//               {/* className = videoplayer container */}
+//           <ReactPlayer url={video.url} controls />
+//             </li>
+//             <li>
+//               {/* className= video info container */}
+//               {/* description */}
+//               {/* cast / info / release time*/}
+//             </li>
+//             {/* <li>
+//                more titles (stretch) 
+//             </li> */}
+//           </ul>
+//         </div>
+//       ) : (
+//         <p>Video not found</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default VideoPlayer;
